9af745bb2dfffd8fa9cac3a16ea2578fcf1bc5d2
Rafael Ruiz <rafael.ruiz@sisnet360.com>
2024-11-08 11:42:58 +0100

    [FEATURE] The real initial commit.

---
+++ golang/run-linter.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# Import color codes from colors.sh
+source ./scripts/colors.sh
+
+# Ensure that golangci-lint is installed
+if ! command -v golangci-lint &> /dev/null
+then
+    echo -e "${RED}\ngolangci-lint could not be found. Please install it to proceed.${RESET}"
+    exit 1
+fi
+
+# Run golangci-lint across the entire repository (all Go files)
+echo -e "${BOLD}\nRunning golangci-lint on all Go files in the repository...${RESET}"
+
+OUTPUT=$(golangci-lint run --color always ./...)
+
+LINT_FILES=$(grep -o 'src[^ ]*\.go' <<< "$OUTPUT")
+# Check if no files contains errors
+if [ -z "$LINT_FILES" ]; then
+    echo -e "${GREEN}\nNo files contains errors. Ready to commit.${RESET}\n"
+    exit 0
+fi
+
+echo -e "${YELLOW}\nError report:${RESET} \n\n$OUTPUT"
+
+STAGED_FILES=$(git diff --name-only --cached --diff-filter=AM)
+# Check if no files are staged
+if [ -z "$STAGED_FILES" ]; then
+    echo -e "${RED}\nNo files are staged for commit.${RESET}\n"
+    exit 1
+fi
+
+echo -e "${YELLOW}\nChecking stagged files:\n${RESET}"
+
+HAS_ERRORS=false
+while IFS= read -r file; do
+    # Check if the file is in the list of staged files
+    if grep -q "^$file$" <<< "$STAGED_FILES"; then
+        echo -e "File '${BOLD}$file${RESET}' contains errors."
+        HAS_ERRORS=true
+    fi
+done <<< "$LINT_FILES"
+
+# Check the boolean variable after the loop
+if [ "$HAS_ERRORS" = true ]; then
+    echo -e "${RED}\nError: At least one staged file contains errors, fix it before commit.${RESET}"
+    exit 1
+fi
+
+echo -e "${GREEN}\nAll Go files passed the linter. Ready to commit.${RESET}"
---
+++ scripts/colors.sh
@@ -0,0 +1,6 @@
+# Color codes
+BOLD="\033[1m"
+RED="\033[0;31m"
+GREEN="\033[0;32m"
+YELLOW="\033[0;33m"
+RESET="\033[0m"
