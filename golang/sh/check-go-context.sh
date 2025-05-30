6a233f21d64702b7438db9d82f0a3ae42be55910
Rafael Ruiz <rafaelmostoles24595@gmail.com>
2025-05-30 12:58:46 +0200

    [FEATURE] Add control flags to check-go-context.

--- golang/sh/check-go-context.sh
+++ golang/sh/check-go-context.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 _VERSION="1.0.0"
 
@@ -9,13 +10,42 @@ source ./scripts/colors.sh
 # shellcheck disable=SC1091
 source ./golang/sh/utils-golang.sh
 
-echo -e "\nChecking Golang context...\n"
+PROJECT=false
+STAGED=false
+INSTALL=false
 
-echo -e "Verifying if this is a Go project..."
-ensure_go_project && echo -e "${GREEN}Go project verified.${RESET}"
+if [ "$#" -eq 0 ];then
+    PROJECT=true
+    STAGED=true
+    INSTALL=true
+fi
 
-echo -e "\nChecking for staged Go files..."
-ensure_go_stagged && echo -e "${GREEN}Staged Go files found.${RESET}"
+# Parse args
+for arg in "$@"; do
+    if [ "$arg" == "--project" ] || [ "$arg" == "-p" ]; then
+        PROJECT=true
+    fi
+    if [ "$arg" == "--staged" ] || [ "$arg" == "-s" ]; then
+        STAGED=true
+    fi
+    if [ "$arg" == "--install" ] || [ "$arg" == "-i" ]; then
+        INSTALL=true
+    fi
+done
 
-echo -e "\nVerifying if Go is installed..."
-ensure_go_installed && echo -e "${GREEN}Go is installed.${RESET}"
+echo -e "\nChecking Golang context..."
+
+if $PROJECT; then
+    echo -e "\nVerifying if this is a Go project..."
+    ensure_go_project && echo -e "${GREEN}\nGo project verified.${RESET}"
+fi
+
+if $STAGED; then
+    echo -e "\nChecking for staged Go files..."
+    ensure_go_staged && echo -e "${GREEN}\nStaged Go files found.${RESET}"
+fi
+
+if $INSTALL; then
+    echo -e "\nVerifying if Go is installed..."
+    ensure_go_installed && echo -e "${GREEN}\nGo is installed.${RESET}"
+fi
