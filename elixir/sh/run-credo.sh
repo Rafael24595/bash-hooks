84d3de8b5954f085f0e8b491ee361548ab8d8997
Rafael Ruiz <rafaelmostoles24595@gmail.com>
2025-09-03 00:53:16 +0200

    [FEATURE] Compare staged files list with Credo flycheck report.

--- elixir/sh/run-credo.sh
+++ elixir/sh/run-credo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-_VERSION="1.0.0"
+_VERSION="1.1.0"
 _PACKAGE="run-credo"
 _DETAILS="Run Credo analysis tool."
 
@@ -16,17 +16,39 @@ for arg in "$@"; do
     fi
 done
 
+STAGED_FILES=$(git diff --name-only --cached --diff-filter=ACMR)
+if [ -z "$STAGED_FILES" ]; then
+    echo -e "${GREEN}\nNo valid files are staged for checking.${RESET}\n"
+    exit 0
+fi
+
 echo -e "${BOLD}\nRunning the Credo (Elixir) analysis tool...\n${RESET}"
 
 # Run Credo
 # --strict: Include all issues. Without it, only positive-priority issues (↑ ↗ →) will be reported
 mix credo "$STRICT"
 
-TEST_EXIT_CODE=$?
+# Run Credo
+# --format flycheck: Display the list in a resumed line format
+OUTPUT=$(mix credo "$STRICT" --format flycheck)
+if [ -z "$OUTPUT" ]; then
+    echo -e "${GREEN}\nNo issues detected. Ready to commit.${RESET}\n"
+    exit 0
+fi
 
-if [ "$TEST_EXIT_CODE" -ne 0 ]; then
-    echo -e "${RED}\nError: Some issues have been found. Please fix them before committing.${RESET}"
-    exit $TEST_EXIT_CODE
+echo -e "${YELLOW}\nChecking staged files:\n${RESET}"
+
+HAS_ERRORS=false
+while IFS= read -r FILE; do
+    if grep -q "^$FILE.*$" <<< "$OUTPUT"; then
+        echo -e "File '${BOLD}$FILE${RESET}' contains errors."
+        HAS_ERRORS=true
+    fi
+done <<< "$STAGED_FILES"
+
+if [ "$HAS_ERRORS" = true ]; then
+    echo -e "${RED}\nError: At least one staged file contains errors. Please fix them before committing.${RESET}"
+    exit 1
 fi
 
 echo -e "${GREEN}\nNo issues found! Ready to commit.${RESET}"
