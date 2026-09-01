930f00d14387dec4e76a13aaabe7a177e5c2028f
Rafael Ruiz <rafaelmostoles24595@gmail.com>
2026-09-02 00:08:48 +0200

    feat: add coverage analysis support to run-tests script

--- README.md
+++ README.md
@@ -268,6 +268,7 @@ By default, the script will clean the resources directory of the workspace where
 >       - **Flags**: 
 >           - **--tags=`<tag>`** | **--t=`<tag>`**: Defines the test build tags. Can be specified multiple times. Tags are combined as a comma-separated list.
 >           - **--race** | **--r**: Runs the tests using the race detector.
+>           - **--cover=`<threshold>`** | **--c=`<threshold>`**: Runs the tests with coverage analysis. If a threshold is specified, the test will fail if the coverage is below that value. The threshold can be defined as a percentage (e.g., `80.0`) or as an absolute number of statements (e.g., `100`).
 >    - **run-govulncheck.sh**: Executes `govulncheck` to check for vulnerabilities in dependencies.
 >       - **Flags**: 
 >           - **--install**: Installs the latest version of govulncheck if it is not already installed
--- golang/sh/run-tests.sh
+++ golang/sh/run-tests.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-_VERSION="1.5.0"
+_VERSION="1.6.0"
 _PACKAGE="run-tests"
 _DETAILS="Run all Go tests."
 
 # Import color codes from colors.sh
 # shellcheck disable=SC1091
 source ./scripts/colors.sh
+# Import utility functions from math.sh
+# shellcheck disable=SC1091
+source ./tools/sh/math.sh
 
 RACE=false
 TAGS=()
+
+COVERAGE_ENABLED=false
+COVERAGE_THRESHOLD=0
 
 for FLAG in "$@"; do
     case "$FLAG" in
@@ -19,16 +25,40 @@ for FLAG in "$@"; do
         --race | --r)
             RACE=true
             ;;
+        --cover | --c)
+            COVERAGE_ENABLED=true
+            ;;
+        --cover=* | --c=*)
+            COVERAGE_ENABLED=true
+            COVERAGE_THRESHOLD="${FLAG#*=}"
+            ;;
     esac
 done
+
+if ! [[ "$COVERAGE_THRESHOLD" =~ ^([0-9]+([.][0-9]+)?)$ ]] || 
+    is_float_less_than "$COVERAGE_THRESHOLD" 0 || 
+    is_float_greater_than "$COVERAGE_THRESHOLD" 100; then
+        echo -e "${RED}\n  ✗ Invalid coverage threshold: ${COVERAGE_THRESHOLD}%${RESET}"
+        exit 1
+fi
 
 # Run Go tests on all the Go files in the project
 echo -e "${BOLD}\nRunning Go tests...\n${RESET}"
 
 ARGS=(-v -failfast ./...)
+
 if [ "$RACE" = true ]; then
     echo -e "  ${GREEN}✓ Race detection enabled.${RESET}"
     ARGS=(-race "${ARGS[@]}")
+fi
+
+if [ "$COVERAGE_ENABLED" = true ]; then
+    echo -e "  ${GREEN}✓ Coverage reporting enabled.${RESET}"
+    ARGS=(-coverprofile=coverage.out "${ARGS[@]}")
+fi
+
+if is_float_greater_than "$COVERAGE_THRESHOLD" "0"; then
+    echo -e "  ${GREEN}✓ Coverage threshold: ${YELLOW}${COVERAGE_THRESHOLD}%${RESET}"
 fi
 
 if (( ${#TAGS[@]} > 0 )); then
@@ -51,29 +81,48 @@ printf "%b\n" "$OUTPUT"
 
 echo -e "${BOLD}\nTotal tests run: ${YELLOW}${TOTAL_TESTS}${RESET}"
 
-if [ $TEST_EXIT_CODE -eq 0 ]; then
-    echo -e "${GREEN}\nAll tests passed! Ready to commit.${RESET}"
-    exit 0
-fi
+if [ $TEST_EXIT_CODE -ne 0 ]; then
+   FAILED_TESTS=$(echo "$OUTPUT" | grep '^--- FAIL:' | awk '{print $3}')
 
-FAILED_TESTS=$(echo "$OUTPUT" | grep '^--- FAIL:' | awk '{print $3}')
-if [ -z "$FAILED_TESTS" ]; then
-    echo -e "${RED}\nNo specific failed tests found.${RESET}"
+    if [ -z "$FAILED_TESTS" ]; then
+        echo -e "${RED}\nNo specific failed tests found.${RESET}"
+        exit $TEST_EXIT_CODE
+    fi
+
+    echo -e "${RED}\nSome tests failed. Please fix them before committing.${RESET}"
+
+    FAILED_PACKAGES=$(echo "$OUTPUT" | grep '^FAIL\s' | awk '{print $2}')
+
+    echo -e "${RED}\nFailed tests ($(echo "$FAILED_TESTS" | wc -l)):${RESET}"
+
+    # shellcheck disable=SC2001
+    echo "$FAILED_TESTS" | sed 's/^/  - /'
+
+    echo -e "${RED}\nFailed packages ($(echo "$FAILED_PACKAGES" | wc -l)):${RESET}"
+
+    # shellcheck disable=SC2001
+    echo "$FAILED_PACKAGES" | sed 's/^/  - /'
+
     exit $TEST_EXIT_CODE
 fi
 
-echo -e "${RED}\nSome tests failed. Please fix them before committing.${RESET}"
+COVERAGE_PERCENT=0
+if [ "$COVERAGE_ENABLED" = true ] && [ -f coverage.out ]; then
+    COVERAGE_PERCENT=$(go tool cover -func=coverage.out | grep total | awk '{print $3}')
+    echo -e "${BOLD}Code coverage: ${YELLOW}${COVERAGE_PERCENT}${RESET}"
+fi
 
-echo -e "${RED}\nFailed tests ($(echo "$FAILED_TESTS" | wc -l)):${RESET}"
+if is_float_greater_than "$COVERAGE_THRESHOLD" "0" && [ -n "$COVERAGE_PERCENT" ]; then
+    COVERAGE_VALUE="${COVERAGE_PERCENT%\%}"
 
-# shellcheck disable=SC2001
-echo "$FAILED_TESTS" | sed 's/^/  - /'
+    if is_float_less_than "$COVERAGE_VALUE" "$COVERAGE_THRESHOLD"; then
+        echo -e "${RED}\nCode coverage ${COVERAGE_PERCENT} is below the required threshold of ${COVERAGE_THRESHOLD}%.${RESET}"
+        exit 1
+    else
+        echo -e "${GREEN}\nCode coverage ${COVERAGE_PERCENT} meets the required threshold of ${COVERAGE_THRESHOLD}%.${RESET}"
+    fi
+fi
 
-FAILED_PACKAGES=$(echo "$OUTPUT" | grep '^FAIL\s' | awk '{print $2}')
+echo -e "${GREEN}\nAll tests passed! Ready to commit.${RESET}"
+exit 0
 
-echo -e "${RED}\nFailed packages ($(echo "$FAILED_PACKAGES" | wc -l)):${RESET}"
-
-# shellcheck disable=SC2001
-echo "$FAILED_PACKAGES" | sed 's/^/  - /'
-
-exit $TEST_EXIT_CODE
